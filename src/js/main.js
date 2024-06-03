cf6bdb543f585bffdeccbf352b8a3693babb98e2
Yakup <yakupjoraev@gmail.com>
2024-06-03 13:24:38 +0300

    added modals wrapper

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,39 @@ function profilePortfolioSlider() {
 }
 
 profilePortfolioSlider();
+
+function modals() {
+
+  const openModalBtns = document.querySelectorAll('.open-modal-btn');
+  const closeModalBtns = document.querySelectorAll('.modal__close');
+  const modals = document.querySelectorAll('.modal');
+  const body = document.querySelector('body')
+
+  openModalBtns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      const modalId = btn.dataset.modalId;
+      const modal = document.getElementById(modalId);
+      modal.classList.add('show');
+      body.classList.add('locked');
+    });
+  });
+
+  closeModalBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const modal = btn.closest('.modal');
+      modal.classList.remove('show');
+      body.classList.remove('locked');
+    });
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal')) {
+      event.target.classList.remove('show');
+      body.classList.remove('locked');
+    }
+  });
+
+}
+
+modals();
